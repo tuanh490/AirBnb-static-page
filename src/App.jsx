3ed9c3d4ce23eb61ecf4e2eeb82371b0e9181c55
Tu Anh <ntuanh490@gmail.com>
2023-05-09 23:59:48 +0700

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,16 @@ export default function App() {
             <Hero />
             <section className="card-container">
                 {cards}
+                <Card
+                    key={1}
+                    openSpots={0}
+                    img="/katie-zaferes.png"
+                    rating="5.0"
+                    reviewCount={2}
+                    location="Online"
+                    title="cool"
+                    price={12}
+                />
             </section>
         </>
     )
